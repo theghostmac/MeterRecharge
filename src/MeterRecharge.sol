946921893ac66ea075d71353447f7cb560854478
theghostmac <theghostmac@gmail.com>
2024-03-17 10:05:04 +0100

    [feature] started writing the smart contract

--- src/MeterRecharge.sol
+++ src/MeterRecharge.sol
@@ -1,8 +1,25 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
 contract MeterRecharge {
-    constructor(){
 
+    // Event declaration for recharge actions.
+    event Recharged(address indexed user, uint256 ethAmount, uint256 powerUnits);
+
+    // Conversion rate from ETH to power units.
+    uint256 public conversionRate;
+
+    constructor(uint256 _initialConversionRate){
+        conversionRate = _initialConversionRate;
+    }
+
+    // Function to update the conversion rate (only by the owner).
+    function updateConversionRate(uint256 _newRate) external{
+        conversionRate = _newRate;
+    }
+
+    // Function to recharge the meter.
+    function recharge() external payable {
+        require(msg.value > 0)
     }
 }
